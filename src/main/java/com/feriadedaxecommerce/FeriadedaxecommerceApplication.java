5e2815af86da3aed67c974f4d4604b59cc3dd4ee
Joey <joey.frimin@soprabanking.com>
2023-04-13 19:26:06 +0200

    [INITIAL COMMIT]

---
+++ src/main/java/com/feriadedaxecommerce/FeriadedaxecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.feriadedaxecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeriadedaxecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeriadedaxecommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feriadedaxecommerce/FeriadedaxecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feriadedaxecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeriadedaxecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
